33f84878a649382c8f1458734e639a905fab3abe
Tamoy <sinclairtamoy@gmail.com>
2022-03-14 21:08:40 -0400

    commit tree be157f0815e41985339b00025da41e8de54e3c54

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    return Math.abs(block - 42)
+}
+function distanceFromHqInFeet(location){
+  //  return Math.abs((location - 42) * 264)
+  const blocks = distanceFromHqInBlocks(location)
+  return (blocks * 264)
+}
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264
+}
+function calculatesFarePrice(start, destination){
+    // calculated distance in feet
+    let distance = distanceTravelledInFeet(start, destination)
+    // let price = calculatesFarePrice(start, destination)
+    if (distance <= 400){
+        return 0
+    }
+    if (distance < 2000){
+        return (distance - 400) * .02
+    }
+    // distance is btwn 2000 and 25000
+    if (distance > 2000 && distance <= 2500){
+        return 25
+    }
+    if (distance >= 2500){
+        return 'cannot travel that far'
+    }
+    
+}
